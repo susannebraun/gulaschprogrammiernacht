ebad728fd68f0954674caa7e5909ad0c639a6d8b
Konk <oih>
2012-11-17 12:02:07 +0100

    - test commit

--- src/com/github/susannebraun/gulasch/dummy/DummyContent.java
+++ src/com/github/susannebraun/gulasch/dummy/DummyContent.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 public class DummyContent {
 
     public static class DummyItem {
@@ -30,6 +31,8 @@ public class DummyContent {
         addItem(new DummyItem("1", "Item 1"));
         addItem(new DummyItem("2", "Item 2"));
         addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("4", "Gulaschitem 42")); // git commit test
+        
     }
 
     private static void addItem(DummyItem item) {
